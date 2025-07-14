41dd2f33d04b0b7d7f2ede5c27fa714977be95d5
R0L0ct <facundoalemdev@gmail.com>
2025-07-14 18:08:06 -0300

    add luasnip jsregexp & vim.hl in init file

--- lua/rolo/plugins/luasnip.lua
+++ lua/rolo/plugins/luasnip.lua
@@ -1,6 +1,9 @@
 return {
 	"L3MON4D3/LuaSnip",
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
+	build = "make install_jsregexp",
 	version = "v2.*",
 	main = "rolo.config.plugins.luasnip",
 	config = true,
